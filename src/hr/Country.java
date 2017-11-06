aae5d815a0d5c29bcde61bfd939164ae4deecebc
John Doe <johndoe@example.com>
2017-11-06 14:55:24 +0100

    Conflicto resuelto

--- src/hr/Country.java
+++ src/hr/Country.java
@@ -1,32 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hr;
 
 /**
- *
+ * Clase que representa un país
  * @author usuario
+ * @version 1.0
  */
 public class Country {
     private String countryId;
     private String countryName;
     private Region region;
 
+/**
+ * Constructor vacío
+ */
     public Country() {
     }
 
+/**
+ * Constructor Completo
+ * @param countryId Identificador del país
+ * @param countryName Nombre del país
+ * @param region Objeto de la clase Regions correspondiente al país
+ */
     public Country(String countryId, String countryName, Region region) {
         this.countryId = countryId;
         this.countryName = countryName;
         this.region = region;
     }
 
+/**
+ * Getter del identificador de país
+ * @return Identificador del país
+ */
     public String getCountryId() {
         return countryId;
     }
 
+/**
+ * Setter del identificador de país
+ * @param countryId Identificador del país
+ */
     public void setCountryId(String countryId) {
         this.countryId = countryId;
     }
